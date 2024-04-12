0ad5a2948769b80812daf8d8a3b472c4e908ed78
Johan Bautista <j.bautistaq@uniandes.edu.co>
2024-04-12 17:27:41 -0500

    database settins to GCL

--- files_service/settings.py
+++ files_service/settings.py
@@ -75,10 +75,21 @@ WSGI_APPLICATION = 'files_service.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
+"""DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}"""
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'monitoring_db',
+        'USER': 'monitoring_user',
+        'PASSWORD': 'isis2503',
+        'HOST': '10.128.0.52',
+        'PORT': '',
     }
 }
 
